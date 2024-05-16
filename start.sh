5afcce55c9ffcd9d03195fad979b897af3797532
zxwang <zixiaowang97@gmail.com>
2024-05-16 20:01:48 +0800

    update start.sh and gitignore

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ tune_ckpt_path=llama_7b_0.2_element_moreau_std005_lamb02_lr00002_groupsoft000000
 
 
 echo "[START] - Start Pruning Model"
-python moreauprune.py --pruning_ratio 0.25 --device cpu  --eval_device cuda --block_wise --block_mlp_layer_start 4 --num_examples 10   --block_mlp_layer_end 30 --block_attention_layer_start 4 --block_attention_layer_end 30 --save_ckpt_log_name $prune_ckpt_path --pruner_type taylor --taylor param_first --save_model  --base_model ${base_model} --test_after_train  --iterative_steps 10 --std 0.05 --lamb 0.2 --lr 0.0001 --soft 0.000005
+python moreauprune.py --pruning_ratio 0.25 --device cpu  --eval_device cuda --block_wise --block_mlp_layer_start 4 --num_examples 10   --block_mlp_layer_end 30 --block_attention_layer_start 4 --block_attention_layer_end 30 --save_ckpt_log_name $prune_ckpt_path --pruner_type taylor --taylor param_first --save_model  --base_model ${base_model} --test_after_train  --iterative_steps 10 --std 0.05 --lamb 0.2 --lr 0.0002 --soft 0.000005
 echo "[FINISH] - Finish Pruning Model"
 
 CUDA_VISIBLE_DEVICES=0 bash scripts/evaluate_pretrain.sh ${base_model} prune_log/${prune_ckpt_path} 
